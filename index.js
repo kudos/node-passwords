ddc1a606943b45871c8f7ac669ab2c946fd9b7ac
Jonathan Cremin <jonathan@crem.in>
2014-06-08 23:04:04 +0100

    Handle errors emitted from PBKDF2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ exports.verify = function(password, hash, callback) {
   var iterations = hashParts[2] * 500;
   var salt = hashParts[3];
   var hashed_password = hashParts[4];
-  
+
   pbkdf2(password, salt, iterations, 24, function(error, derivedKey){
     if(error) {
       callback(new Error(error));
@@ -21,21 +21,25 @@ exports.crypt = function(password, cost, callback) {
     callback = cost;
     cost = 2;
   }
-  
+
   iterations = cost * 500;
 
   randomBytes(18, function(error, buf) {
     if(error) {
       callback(new Error(error));
     } else {
-      pbkdf2(password, buf.toString('base64'), iterations, 24, function(error, derivedKey){
-        if(error) {
-          callback(new Error(error));
-        } else {
-          var hash = '$pbkdf2-256-1$' + cost + '$' + buf.toString('base64') + '$' + derivedKey.toString('hex');
-          callback(null, hash);
-        }
-      });
+      try {
+        pbkdf2(password, buf.toString('base64'), iterations, 24, function(error, derivedKey){
+          if(error) {
+            callback(new Error(error));
+          } else {
+            var hash = '$pbkdf2-256-1$' + cost + '$' + buf.toString('base64') + '$' + derivedKey.toString('hex');
+            callback(null, hash);
+          }
+        });
+      } catch(e) {
+        callback(new Error(error));
+      }
     }
   });
 }
