187917ded2b2fc82143402785c59b8d851defbc0
Jonathan Cremin <jonathan@crem.in>
2015-03-02 11:32:57 -0800

    Make tests non-harmony node compatible

--- test/test.js
+++ test/test.js
@@ -68,29 +68,10 @@ describe('passwords', function () {
   });
 
   try {
-    describe('using generators', function () {
-      it('should hash a password', function *() {
-        const hash = yield passwords.crypt('password');
-        assert.equal(5, hash.split('$').length);
-      });
-
-      it('should verify a password', function *() {
-        const hash = passwords.crypt('password');
-        assert(yield passwords.verify('password', hash));
-      });
-
-      it('should reject a password', function *() {
-        const hash = yield passwords.crypt('password');
-        assert(yield !passwords.verify('drowssap', hash));
-      });
-
-      it('should increase cost', function *() {
-        const hash = yield passwords.crypt('password', 10);
-        assert(passwords.verify('password', hash));
-      });
-    });
+    eval("(function *(){})");
+    require('./generators.js');
   } catch(err) {
-    console.log("skipping generators");
+    console.log("no generator support, skipping");
   }
 
 });
