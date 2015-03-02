5e811cea94b52f4a84a1b3da0688e2e937683ad2
Jonathan Cremin <jonathan@crem.in>
2015-03-02 14:43:29 -0800

    Switch hash encoding back to hex

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 
 var passwords = require('..');
 
-var hashPassword = '$pbkdf2-256-1$2$8eKAvmS5GeUBSCk65iRnBNXk$jG3zKXCXMKx3WMJQTmi56+W9BVWyIyj+';
+var hashPassword = '$pbkdf2-256-1$2$XoeBED7r20l03vQcGl8XiTfl$f944bff1cc8e3666ff673e637dc47a85e0bca99f03b8921f';
 
 describe('passwords', function () {
   describe('using callbacks', function () {
